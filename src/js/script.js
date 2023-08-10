ca51c4b6c780b2f24e5dc63a5dd8b835a4a8fdfb
Magdalena Stryszowska <madzi13@op.pl>
2023-08-10 20:58:05 +0200

    update Accordi

--- src/js/script.js
+++ src/js/script.js
@@ -60,16 +60,18 @@ const select = {
       thisProduct.data = data;
   
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
   
       console.log('new Product:', thisProduct);
     }
   
+   
     renderInMenu() {
       const thisProduct = this;
-      
-  
+        
       /* generate HTML based on template */
       const generatedHTML = templates.menuProduct(thisProduct.data);
+      console.log('generated HTML', generatedHTML);
   
       /* create element using utils.createElementFromHTML */
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
@@ -80,9 +82,33 @@ const select = {
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
     }
+    
+    initAccordion(){
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+      
+      /* prevent default action for event */
+      event.preventDefault();
+
+      /* find active product (product that has active class) */
+      const activeProduct = document.querySelector(select.all.menuProductsActive);
+
+      /* if there is active product and it's not thisProduct.element, remove class active from it */
+      if(activeProduct !== null && activeProduct !== thisProduct.element){
+        activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+      }
+      /* toggle active class on thisProduct.element */
+      thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+    });
+    }
   }
-  
-  const app = {
+
+    const app = {
     initMenu: function () {
       const thisApp = this;
       console.log('thisApp.data:', thisApp.data);
