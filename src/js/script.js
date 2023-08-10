2af2a3d38fbd9b2e6f75a66e2fae0545aec54631
Magdalena Stryszowska <madzi13@op.pl>
2023-08-10 21:01:47 +0200

    _1 end Accordion

--- src/js/script.js
+++ src/js/script.js
@@ -92,23 +92,23 @@ const select = {
       /* START: add event listener to clickable trigger on event click */
       clickableTrigger.addEventListener('click', function(event) {
       
-      /* prevent default action for event */
-      event.preventDefault();
+        /* prevent default action for event */
+        event.preventDefault();
 
-      /* find active product (product that has active class) */
-      const activeProduct = document.querySelector(select.all.menuProductsActive);
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
 
-      /* if there is active product and it's not thisProduct.element, remove class active from it */
-      if(activeProduct !== null && activeProduct !== thisProduct.element){
-        activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
-      }
-      /* toggle active class on thisProduct.element */
-      thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
-    });
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if(activeProduct !== null && activeProduct !== thisProduct.element){
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
     }
   }
 
-    const app = {
+  const app = {
     initMenu: function () {
       const thisApp = this;
       console.log('thisApp.data:', thisApp.data);
