61d901f878e60d1ea4e45ac605946f1b2153dda1
Magdalena Stryszowska <madzi13@op.pl>
2023-08-13 18:44:33 +0200

    _Elements

--- src/js/script.js
+++ src/js/script.js
@@ -60,6 +60,7 @@ const select = {
       thisProduct.data = data;
   
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
   
       console.log('new Product:', thisProduct);
@@ -81,6 +82,16 @@ const select = {
   
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
+    }
+
+    getElements(){
+      const thisProduct = this;
+    
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
     }
     
     initAccordion(){
