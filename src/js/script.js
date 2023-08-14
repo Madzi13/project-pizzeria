0b19491b47be068d763354f95899e5683db904ce
Magdalena Stryszowska <madzi13@op.pl>
2023-08-14 16:31:04 +0200

    image

--- src/js/script.js
+++ src/js/script.js
@@ -95,6 +95,7 @@ const select = {
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
     
     initAccordion(){
@@ -161,7 +162,8 @@ const select = {
           const option = param.options[optionId];
           console.log(optionId, option);
          // check if there is param with a name of paramId in formData and if it includes optionId
-         if(formData[paramId] && formData[paramId].includes(optionId)) {
+         const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+        if(optionSelected) {
           // check if the option is not default
           if(!option.default) {
             // add option price to price variable
@@ -172,6 +174,14 @@ const select = {
             if(option.default) {
               // reduce price variable
              price -= option.price;
+            }
+          }
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+          if(optionImage) {
+            if(optionSelected) {
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            } else {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
             }
           }
         }
