07f6a5eaf91d9f8ae2ee2f145361a0a36a58d252
Magdalena Stryszowska <madzi13@op.pl>
2023-09-13 15:46:00 +0200

    Booking_4

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
     import { settings, select, classNames } from './settings.js';
     import Product from './components/Product.js';
     import Cart from './components/Cart.js';
+    import Booking from './components/Booking.js';
     
     const app = {
       initPages: function(){
@@ -102,12 +103,19 @@
         //console.log('thisApp.data', JSON.stringify(thisApp.data));
       },
 
+      initBooking: function(){
+        const thisApp = this;
+        thisApp.bookingPage = document.querySelector(select.containerOf.booking);
+        thisApp.booking = new Booking(thisApp.bookingPage);
+      },
+
       init: function(){
         const thisApp = this;
 
         thisApp.initPages();
         thisApp.initData();
         thisApp.initCart();
+        thisApp.initBooking();
       },
     };
    
