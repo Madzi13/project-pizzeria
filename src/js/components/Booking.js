1034b4c2c2318c3b235dd96f2846400bdb829f75
Magdalena Stryszowska <madzi13@op.pl>
2023-09-19 17:06:45 +0200

    Datepicker

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,6 +1,7 @@
 import { templates, select } from '../settings.js';
 import AmountWidget from './AmountWidget.js';
-
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 class Booking{
     constructor(element){
         const thisBooking = this;
@@ -16,6 +17,8 @@ class Booking{
         thisBooking.dom.wrapper.innerHTML = generatedHTML;
         thisBooking.dom.peopleAmount = element.querySelector(select.booking.peopleAmount);
         thisBooking.dom.hoursAmount = element.querySelector(select.booking.hoursAmount);
+        thisBooking.dom.datePicker = element.querySelector(select.widgets.datePicker.wrapper);
+        thisBooking.dom.hourPicker = element.querySelector(select.widgets.hourPicker.wrapper);
     }
     initWidgets(){
         const thisBooking = this;
@@ -31,6 +34,8 @@ class Booking{
         thisBooking.dom.hoursAmount.addEventListener('updated', function(){
             thisBooking.amount = thisBooking.amountWidget;
           });
+          thisBooking.datePickerWidget = new DatePicker(thisBooking.dom.datePicker);
+          thisBooking.hourPickerWidget = new HourPicker(thisBooking.dom.hourPicker);    
     }
 }
 
